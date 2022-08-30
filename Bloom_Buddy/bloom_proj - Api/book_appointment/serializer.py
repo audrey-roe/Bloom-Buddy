1a3cec56296fe0952057109019d946beec5cd57f
Al-ghazali251 <109509200+Al-ghazali251@users.noreply.github.com>
2022-08-30 02:00:18 +0100

    Delete Bloom_Buddy/bloom_proj - Api directory

--- Bloom_Buddy/bloom_proj - Api/book_appointment/serializer.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import appointment
-
-class appointmentForm(forms.ModelForm):
-
-    class Meta:
-        model = appointment
-        fields = ('appointment_month', 'appointment_date', 'appointment_time', 'full_name', 'age', 'gender')
