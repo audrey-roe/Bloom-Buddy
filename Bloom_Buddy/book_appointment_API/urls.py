9029ce67e2b3228649ca112f0a064aaf2d9cac28
Al-ghazali251 <109509200+Al-ghazali251@users.noreply.github.com>
2022-08-31 07:49:43 +0100

    Delete Bloom_Buddy/book_appointment_API directory

--- Bloom_Buddy/book_appointment_API/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . views import book_appointment
-
-
-urlpatterns = [
-    path('book_appointment',book_appointment.as_view(), name='book_appointment'),
-    
-    
-]
